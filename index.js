f954e2c6ecef0b1fff5c89d8c5eb1671b4ebed6c
alvin <alvin.sarisar@student.moringaschool.com>
2024-07-11 19:31:52 +0300

    taxifare

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-// Code your solution in this file!
-const hq = 42;
 
-// Calculates the distance from HQ in blocks
-function distanceFromHqInBlocks(street) {
-  return Math.abs(street - hq);
+function distanceFromHqInBlocks(someValue) {
+  const hq = 42;
+  return Math.abs(someValue - hq);
 }
 
-// Calculates the distance from HQ in feet (1 block = 264 feet)
-function distanceFromHqInFeet(street) {
-  return distanceFromHqInBlocks(street) * 264;
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
 }
 
-// Calculates the distance traveled in feet between two blocks
 function distanceTravelledInFeet(start, destination) {
   return Math.abs(start - destination) * 264;
 }
 
-// Calculates the fare price for the trip
 function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
-  
+
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
@@ -30,3 +25,4 @@ function calculatesFarePrice(start, destination) {
     return 'cannot travel that far';
   }
 }
+
